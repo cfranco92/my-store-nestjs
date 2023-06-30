c662f7c8cf16071d1c6a401b217b1f27451c101b
Cristian Franco <cfrancobedoya@gmail.com>
2023-06-29 20:37:50 -0500

    [CI-CD] Add categories controller main structure

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('categories')
 export class CategoriesController {
-  @Get('/:categoryId/products/:productId')
-  getProductByCategory(
-    @Param('productId') productId: string,
-    @Param('categoryId') categoryId: string,
-  ) {
-    return `product ${productId} and category ${categoryId}`;
+  // constructor() {}
+
+  @Get()
+  getCategories() {
+    return [];
+  }
+
+  @Get(':categoryId')
+  getOne() {
+    return {};
+  }
+
+  @Post()
+  create() {
+    return {};
+  }
+
+  @Put(':id')
+  update() {
+    return {};
+  }
+
+  @Delete(':id')
+  delete() {
+    return {};
   }
 }
