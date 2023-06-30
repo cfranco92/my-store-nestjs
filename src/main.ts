6c0edf96e90995ed625db3664c4667ebf58aba89
Cristian Franco <cfrancobedoya@gmail.com>
2023-06-29 19:39:21 -0500

    [CI-CD] Avoid incorrect parameters with whitelist and forbidNonWhitelisted

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // This will remove any extra properties that are not in the DTO
+      forbidNonWhitelisted: true, // This will throw an error if there are extra properties that are not in the DTO
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
